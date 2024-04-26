7241130f1c160ad23d428f37bc090e504bd1c699
Fermazate <fernandomazate@gmail.com>
2024-04-26 12:22:25 -0600

    CHANGED: Changed way of importing views. Since urls.py uses all the views, imported all the module views.

--- backend/healthia/urls.py
+++ backend/healthia/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import UserCreate, Logout, UserQueryViewSet, ChatResponseViewSet
+from . import views
 
 router = DefaultRouter()
-router.register(r"userqueries", UserQueryViewSet)
-router.register(r"chatresponse", ChatResponseViewSet)
+router.register(r"userqueries", views.UserQueryViewSet)
+router.register(r"chatresponse", views.ChatResponseViewSet)
 
 urlpatterns = [
     path("", include(router.urls)), #DRF router url)
-    path("register/", UserCreate.as_view(), name="register"), #Registration url
+    path("register/", views.UserCreate.as_view(), name="register"), #Registration url
     path("login/", obtain_auth_token, name="login"), #DRF login url
-    path("logout/", Logout.as_view(), name="logout"), #Logout url)
+    path("logout/", views.Logout.as_view(), name="logout"), #Logout url)
 ]
